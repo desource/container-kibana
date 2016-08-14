b78543cb36d3071abb063d41c333f2a935e35ce9
Markus Kobler <markus.kobler@digital.hmrc.gov.uk>
2016-08-14 16:41:44 +0100

    Updating build for concourse

--- build.sh
+++ build.sh
@@ -6,47 +6,32 @@ KIBANA_SHA1=355c631b77c529d3dea304d7f084e658f5cc3123
 
 BASE=$PWD
 SRC=$PWD/src
-OUT=$PWD/kibana-build
+OUT=$PWD/out
 ROOTFS=$PWD/rootfs
 
-mkdir -p $OUT
-mkdir -p $ROOTFS
-
-curl -sL https://github.com/gliderlabs/docker-alpine/blob/rootfs/library-edge/versions/library-edge/rootfs.tar.gz?raw=true -o rootfs.tar.gz
-
-tar -xf rootfs.tar.gz -C $ROOTFS
-
-mv $ROOTFS/etc/localtime $ROOTFS/usr/share/zoneinfo
-ln -s /usr/share/zoneinfo $ROOTFS/etc/localtime 
-
-mkdir -p $ROOTFS/opt/kibana
+mkdir -p $OUT $ROOTFS/opt/kibana
 cd $BASE
-curl -sL https://download.elastic.co/kibana/kibana/kibana-$KIBANA_VERSION-linux-x64.tar.gz -o kibana-$KIBANA_VERSION-linux-x64.tar.gz
+curl -sOL https://download.elastic.co/kibana/kibana/kibana-$KIBANA_VERSION-linux-x64.tar.gz
 echo "$KIBANA_SHA1  kibana-$KIBANA_VERSION-linux-x64.tar.gz" | sha1sum -c
 tar -xf kibana-$KIBANA_VERSION-linux-x64.tar.gz -C $ROOTFS/opt/kibana --strip-components 1
 
 rm -rf $ROOTFS/opt/kibana/node $ROOTFS/opt/kibana/bin
 
-cp -r $SRC/etc $ROOTFS
-
 cd $ROOTFS
 tar -cf $OUT/rootfs.tar .
 
 cat <<EOF > $OUT/Dockerfile
-FROM scratch
+FROM quay.io/desource/nodejs
 
 ADD rootfs.tar /
 
-RUN apk add --no-cache nodejs
-
-ENV NODE_ENV=production
-
 EXPOSE 5601
 
-WORKDIR /opt/kibana
+WORKDIR /data
+
+USER nobody
 
 ENTRYPOINT ["/usr/bin/node", "src/cli"]
 
 EOF
 
-# USER nobody
